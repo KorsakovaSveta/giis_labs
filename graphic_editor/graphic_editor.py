d95bf883e4a6ebb71e2f5f8773d8bc1e611dcfaa
KorsakovaSveta <swe123ta@gmail.com>
2024-04-03 18:02:17 +0300

    add labs 2, 3, 4

--- graphic_editor/graphic_editor.py
+++ graphic_editor/graphic_editor.py
@@ -24,22 +24,45 @@ class GraphicEditor:
         self.mainmenu.add_cascade(label="Кривая Безье", command=self.activate_canvas_curve_Bezie)
         self.mainmenu.add_cascade(label="Кривая Эрмита", command=self.activate_canvas_curve_Hermit)
         self.mainmenu.add_cascade(label="В-сплайн", command=self.activate_canvas_B_splain)
-        
+        self.mainmenu.add_cascade(label="Куб", command=self.activate_canvas_draw_cube)
+        self.mainmenu.add_cascade(label="Перемещение", command=self.activate_canvas_translate_cube)
+        self.mainmenu.add_cascade(label="Поворот", command=self.activate_canvas_rotate_cube)
+        self.mainmenu.add_cascade(label="Скалирование", command=self.activate_canvas_scale_cube)
+        self.mainmenu.add_cascade(label="Отображение", command=self.activate_canvas_reflect_cube)
+        self.mainmenu.add_cascade(label="Перспектива", command=self.activate_canvas_perspective_cube)
         #self.cda = tk.Button(self.window, text="Отрезки", command=self.activate_canvas)
         self.mainmenu.add_cascade(label="Отладка", command=self.debug_mode_toggle)
         self.mainmenu.add_cascade(label="Остановить отладку", command=self.delete_grid)
+        self.mainmenu.add_cascade(label="Корректировка", command=self.activate_canvas_editing)
+        self.mainmenu.add_cascade(label="Остановить корректировку", command=self.end_editing)
+        self.mainmenu.add_cascade(label="Очистить экран", command=self.clear_window)
         #self.canvas.create_line(0, 2, self.window.winfo_screenwidth(), 2, fill="gray")
         #self.cda.pack()   
         #self.canvas.bind("<Button-1>", self.on_mouse_click)     
-  
-        
+        self.center = np.array([self.canvas.winfo_width() / 2, self.canvas.winfo_height() / 2, 0, 1])
+        self.new_point = None
+        self.changing_and_new_point = []
+        self.dragging = False
         self.canvas.pack()
-       
+        self.Bezie_editing = False
+        self.Hermit_editing = False
+        self.B_splain_editing = False
         self.points = []
         self.derivatives = []
         self.debug_mode = False
         self.grid_size = 10
-
+        self.selected_point = None
+        self.points_for_change = []
+        self.all_points=[]
+        self.for_all_points = []
+        self.translateCube = False
+        self.rotateCube = False
+        self.cube_size = 0
+        self.rotation_angle = 0
+        self.scaleCube = False
+        self.reflectCube = False
+        self.perspectiveCube = False
+        self.exit = False
 
     def activate_canvas_cda(self):
         self.canvas.bind('<Button-1>', self.on_mouse_click_cda)
@@ -63,20 +86,102 @@ class GraphicEditor:
         self.canvas.bind('<Button-1>', self.on_mouse_click_parabola)
 
     def activate_canvas_curve_Bezie(self):
+        self.Bezie_editing = True
         self.canvas.bind('<Button-1>', self.on_mouse_click_curve_Bezie)
         
     def activate_canvas_curve_Hermit(self):
+        self.Hermit_editing = True
         self.canvas.bind('<Button-1>', self.on_mouse_click_curve_Hermit)
-
+        
     def activate_canvas_B_splain(self):
+        self.B_splain_editing = True
         self.canvas.bind('<Button-1>', self.on_mouse_click_B_splain)
+
+    def activate_canvas_draw_cube(self):
+        self.canvas.bind('<Button-1>', self.on_mouse_click_draw_cube)
+
+    def activate_canvas_translate_cube(self):
+        self.canvas.bind('<Button-1>', self.on_mouse_click_translate_cube)
+
+    def activate_canvas_rotate_cube(self):
+        self.window.bind('<KeyPress>', self.on_keybord_click_rotate_cube)
+
+    def activate_canvas_scale_cube(self):
+        self.window.bind('<KeyPress>', self.on_keybord_click_scale_cube)
+    
+    def activate_canvas_reflect_cube(self):
+        self.window.bind('<KeyPress>', self.on_keybord_click_reflect_cube)
+
+    def activate_canvas_perspective_cube(self):
+        self.window.bind('<KeyPress>', self.on_keybord_click_perspective_cube)
+
+    def activate_canvas_editing(self):
+        self.canvas.bind('<Button-1>', self.start_editing)
 
     def debug_mode_toggle(self):
         self.canvas.delete("all")
         self.debug_mode = not self.debug_mode
         self.draw_grid()
-       
-       
+
+    def clear_window(self):
+        self.canvas.delete("all")
+        self.all_points.clear()
+
+    def start_editing(self, event):
+        self.dragging = True  
+        # self.canvas.bind('<Button-1>', self.on_mouse_click_choose_point) 
+        self.changing_and_new_point.append((event.x, event.y)) 
+        for point in self.points_for_change:
+            self.canvas.create_rectangle(point[0]-4, point[1]-4, point[0]+4, point[1]+4, fill="red")
+        if len(self.changing_and_new_point) == 2:
+            
+            changing_point = self.changing_and_new_point[0]
+            for point in self.points_for_change:
+                if point[0]-3<=changing_point[0]<=point[0]+3 or\
+                    point[1]-3<=changing_point[1]<=point[1]+3:  
+                    new_point = self.changing_and_new_point[1]
+                    index_changed_point = self.points_for_change.index(point)
+                    self.points_for_change[index_changed_point] = new_point
+                    if self.Bezie_editing:
+                        
+                        self.canvas.delete("all")
+                        for all in self.all_points:
+                            self.points=all
+                            self.draw_curve_Bezie()
+                        self.points = self.points_for_change
+                        self.draw_curve_Bezie()
+                        self.changing_and_new_point.clear()
+
+                    if self.Hermit_editing:
+                        
+                        self.canvas.delete("all")
+                        for all in self.all_points:
+                            self.points=all
+                            self.draw_curve_Hermit()
+                        self.points = self.points_for_change
+                        self.draw_curve_Hermit()
+                        self.changing_and_new_point.clear()
+                    if self.B_splain_editing:
+                        
+                        self.canvas.delete("all")
+                        for all in self.all_points:
+                            self.points=all
+                            self.draw_B_splain()
+                        self.points = self.points_for_change
+                        self.draw_B_splain()
+                        self.changing_and_new_point.clear()
+
+    def end_editing(self):
+        self.dragging  = False
+        self.Hermit_editing= self.Bezie_editing= self.B_splain_editing=False
+        for i in range(len(self.points)):
+            self.for_all_points.append(self.points[i])   
+        self.all_points.append(self.for_all_points)  
+        self.for_all_points = []
+        #self.canvas.delete("all")
+        self.points.clear()
+        self.changing_and_new_point.clear()    
+
 
     def on_mouse_click_cda(self, event):
         self.points.append((event.x, event.y))
@@ -123,24 +228,83 @@ class GraphicEditor:
     def on_mouse_click_curve_Bezie(self, event):
         self.points.append((event.x, event.y))
         if len(self.points) == 4:           
-            self.draw_curve_Bezie()           
-            self.points = []
+            self.draw_curve_Bezie()          
+            if self.dragging == False:
+                self.points_for_change = self.points
+                self.points = []
+            
 
     def on_mouse_click_curve_Hermit(self, event):
         self.points.append((event.x, event.y))
         if len(self.points) ==2:   
             #self.calculate_derivatives()        
-            self.draw_curve_Hermit()           
-            self.points = []
+            self.draw_curve_Hermit()   
+            if self.dragging == False:
+                self.points_for_change = self.points
+                self.points = []        
+            
 
     def on_mouse_click_B_splain(self, event):
         self.points.append((event.x, event.y))
         if len(self.points) ==4:   
             #self.calculate_derivatives()        
             self.draw_B_splain()           
+            if self.dragging == False:
+                self.points_for_change = self.points
+                self.points = []
+    
+    def on_mouse_click_draw_cube(self, event):
+        
+        self.points.append((event.x, event.y))
+        if len(self.points) ==2:   
+            #self.calculate_derivatives()        
+            self.draw_cube() 
+            self.points_rotate = self.points     
             self.points = []
     
+    def on_mouse_click_translate_cube(self, event):
+        
+        self.points.append((event.x, event.y))
+        
+            #self.calculate_derivatives()        
+        self.translate_cube()   
+        self.points_rotate[0] = self.points[0]        
+        self.points = []
+
+    def on_keybord_click_rotate_cube(self, event):
+        if event.keysym == "Left":
+            self.rotation_angle += 10
+        elif event.keysym == "Right":
+            self.rotation_angle -= 10
+              
+        self.rotate_cube() 
+
+    def on_keybord_click_scale_cube(self, event):
+        if event.keysym == "Up":
+            self.cube_size += 10
+            #self.points_rotate[0]=tuple(num+10 for num in self.points_rotate[0])
             
+        elif event.keysym == "Down":
+            self.cube_size -= 10
+            #self.points_rotate[0]=tuple(num-10 for num in self.points_rotate[0])
+            
+        self.scale_cube() 
+
+    def on_keybord_click_reflect_cube(self, event):
+        if event.keysym == "r":
+            self.reflect_cube()
+        elif event.keysym == "e":
+            self.exit = True
+            self.points = self.points_rotate
+            self.canvas.delete("all")            
+            self.draw_cube()
+            self.points = []
+            self.exit = False
+
+    # def on_keybord_click_perspective_cube(self, event):
+    #     if event.keysym == "p":
+    #         self.perspective_transform()
+        
     def draw_line_cda(self):
         #self.canvas.create_line(0, 2, self.window.winfo_screenwidth(), 2, fill="gray")
         #self.canvas.bind("<Button-1>", self.on_mouse_click)
@@ -446,11 +610,15 @@ class GraphicEditor:
         self.canvas.create_oval(xc-5, yc-5, xc+5, yc+5, fill="red")
 
     def draw_curve_Bezie(self):
+       
+        
+        for point in self.points:
+            self.canvas.create_rectangle(point[0]-4, point[1]-4, point[0]+4, point[1]+4, fill="red")
         x0, y0 = self.points[0]
         x1, y1 = self.points[1]
         x2, y2 = self.points[2]
         x3, y3 = self.points[3]
-       
+        
         #t_arr = np.array([pow(t,3), t*t, t, 1])
         arr=np.array([[-1,3,-3,1],
                       [3,-6,3,0],
@@ -477,6 +645,8 @@ class GraphicEditor:
     #         self.derivatives.append((dx, dy))
 
     def draw_curve_Hermit(self):
+        for point in self.points:
+            self.canvas.create_rectangle(point[0]-4, point[1]-4, point[0]+4, point[1]+4, fill="red")
         p0= self.points[0]
         p1 = self.points[1]
         v0 = [0, 600]
@@ -489,6 +659,8 @@ class GraphicEditor:
             self.canvas.create_rectangle(x, y, x, y)
 
     def draw_B_splain(self):
+        for point in self.points:
+            self.canvas.create_rectangle(point[0]-4, point[1]-4, point[0]+4, point[1]+4, fill="red")
         x1, y1 = self.points[0]
         x2, y2 = self.points[1]
         x3, y3 = self.points[2]
@@ -508,10 +680,7 @@ class GraphicEditor:
         for i in range(3):
             
             p=np.dot(arr,p_arr)
-            self.canvas.create_rectangle(x1, y1, x1+5, y1+5, fill="red")
-            self.canvas.create_rectangle(x2, y2, x2+5, y2+5, fill="red")
-            self.canvas.create_rectangle(x3, y3, x3+5, y3+5, fill="red")
-            self.canvas.create_rectangle(x4, y4, x4+5, y4+5, fill="red")
+            
             
             for t in range(0, 1001, 1):
                 t = t / 1000.0
@@ -523,6 +692,148 @@ class GraphicEditor:
                 self.canvas.create_rectangle(dot[0], dot[1], dot[0], dot[1], fill="black")
             p_arr=np.roll(p_arr,-1,axis=0)
 
+    def draw_cube(self):
+        
+        if self.translateCube == False and\
+            self.rotateCube == False and\
+                self.scaleCube == False and\
+                    self.reflectCube == False:
+                        
+            x, y = self.points[0]
+            if self.exit == False:
+                self.cube_size = abs(self.points[1][1]-self.points[0][1])
+            x2, y2 = self.points[1]
+        elif self.translateCube == True:
+            x, y = self.points[0]
+        elif self.rotateCube == True:
+            x,y = self.points_rotate[0]
+        elif self.scaleCube == True: 
+            x,y = self.points_rotate[0]
+            
+        if self.reflectCube == True:
+            x,y = self.points_rotate[0]
+            vertices = [
+            (x - self.cube_size, y - self.cube_size),
+            (x + self.cube_size, y - self.cube_size),
+            (x + self.cube_size, y + self.cube_size),
+            (x - self.cube_size, y + self.cube_size),
+            (x - self.cube_size + 50, y - self.cube_size - 50),
+            (x + self.cube_size + 50, y - self.cube_size - 50),
+            (x + self.cube_size + 50, y + self.cube_size - 50),
+            (x - self.cube_size + 50, y + self.cube_size - 50)
+        ]    
+        else:
+        # Вершины куба
+            vertices = [
+            (x - self.cube_size, y - self.cube_size),
+            (x + self.cube_size, y - self.cube_size),
+            (x + self.cube_size, y + self.cube_size),
+            (x - self.cube_size, y + self.cube_size),
+            (x - self.cube_size - 50, y - self.cube_size - 50),
+            (x + self.cube_size - 50, y - self.cube_size - 50),
+            (x + self.cube_size - 50, y + self.cube_size - 50),
+            (x - self.cube_size - 50, y + self.cube_size - 50)
+        ]
+    
+    # Задаем грани куба
+        edges = [
+            (0, 1), (1, 2), (2, 3), (3, 0),
+            (4, 5), (5, 6), (6, 7), (7, 4),
+            (0, 4), (1, 5), (2, 6), (3, 7)
+        ]
+        if self.rotateCube == True:
+            rotation_matrix = [
+                [math.cos(math.radians(self.rotation_angle)), -math.sin(math.radians(self.rotation_angle))],
+                [math.sin(math.radians(self.rotation_angle)), math.cos(math.radians(self.rotation_angle))]
+            ]
+       
+        # Поворот вершин куба
+            for i in range(len(vertices)):
+                x = vertices[i][0] - self.points_rotate[0][0]
+                y = vertices[i][1] - self.points_rotate[0][1]
+                new_x = x * rotation_matrix[0][0] + y * rotation_matrix[0][1]
+                new_y = x * rotation_matrix[1][0] + y * rotation_matrix[1][1]
+                vertices[i] = (new_x + self.points_rotate[0][0], new_y + self.points_rotate[0][1])
+                
+       
+            
+        # Рисуем куб 
+        if self.perspectiveCube == False:
+            for edge in edges:
+                x1, y1 = vertices[edge[0]]
+                x2, y2 = vertices[edge[1]]
+                self.canvas.create_line(x1, y1, x2, y2)
+
+    def translate_cube(self):
+        self.translateCube = True
+        
+        self.canvas.delete("all")
+        #x, y = self.points[0]
+        self.draw_cube()
+        self.translateCube = False
+
+    def rotate_cube(self):
+        self.rotateCube = True
+        self.canvas.delete("all")
+        #x, y = self.points[0]
+        self.draw_cube()
+        self.rotateCube = False
+
+    def scale_cube(self):
+        self.scaleCube = True
+        self.canvas.delete("all")
+        self.draw_cube()
+        self.scaleCube = False
+
+    def reflect_cube(self):
+        self.reflectCube = True
+        self.canvas.delete("all")
+        self.draw_cube()
+        self.reflectCube = False
+
+    # def perspective_transform(self):
+    #     self.perspectiveCube = True
+    #     self.canvas.delete("all")
+    #     x,y = self.points_rotate[0]
+       
+    #     vertices = [
+    #     self.transform_point(x - self.cube_size, y - self.cube_size, 0.01),
+    #     self.transform_point(x + self.cube_size, y - self.cube_size, 0.01),
+    #     self.transform_point(x + self.cube_size, y + self.cube_size, 0.01),
+    #     self.transform_point(x - self.cube_size, y + self.cube_size, 0.01),
+    #     self.transform_point(x - self.cube_size - 50, y - self.cube_size - 50, 0.01),
+    #     self.transform_point(x + self.cube_size - 50, y - self.cube_size - 50, 0.01),
+    #     self.transform_point(x + self.cube_size - 50, y + self.cube_size - 50, 0.01),
+    #     self.transform_point(x - self.cube_size - 50, y + self.cube_size - 50, 0.01)
+    # ]
+    
+    # # Задаем грани куба
+    #     edges = [
+    #         (0, 1), (1, 2), (2, 3), (3, 0),
+    #         (4, 5), (5, 6), (6, 7), (7, 4),
+    #         (0, 4), (1, 5), (2, 6), (3, 7)
+    #     ]
+
+    #     # Применение матрицы проекции к вершинам куба
+    #     for edge in edges:
+    #         x1, y1, _ = vertices[edge[0]]
+    #         x2, y2, _ = vertices[edge[1]]
+    #         self.canvas.create_line(x1, y1, x2, y2)
+
+    #     # Очистка холста
+        
+
+    #     self.perspectiveCube = True
+
+    # def transform_point(self, x, y, z):
+    #     field_of_view = math.radians(60)  # угол обзора в градусах
+    #     aspect_ratio = 1.0  # соотношение сторон экрана
+    #     near = 0.1  # плоскость отсечения близкая
+    #     far = 100.0
+    #     transformed_x = x / (z * math.tan(field_of_view / 2))
+    #     transformed_y = y / (z * math.tan(field_of_view / 2) * aspect_ratio)
+    #     transformed_z = (far + near) / (far - near) - (2 * far * near) / (far - near) / z
+    #     return transformed_x, transformed_y, transformed_z
 
     def draw_grid(self):
         for x in range(0, self.window.winfo_screenwidth(), self.grid_size):
